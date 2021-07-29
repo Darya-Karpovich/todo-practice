d629d11f954ba5e2f0364ccdc78b472efa8cf163
48796 <dashutabrest@gmail.com>
2021-07-29 19:14:03 +0200

    added new features

--- client/js/todo-list.js
+++ client/js/todo-list.js
@@ -1,19 +1,18 @@
-
 const todosListEl = document.getElementById("todos-list");
 const btnAdd = document.getElementById("button-add");
 const formEl = document.getElementById("form-todo");
 
 getAllTodos().then((todos) => {
   for (const todo of todos) {
-    appendTodo(todo);
+    appendTodo(todosListEl, todo);
   }
 });
 
 const paramsRes = new URL(window.location.href).searchParams.get("res");
-if (paramsRes === 'true') {
-  showAlert('Todo was updated successfully')
-} else if (paramsRes === 'false')  {
-  showAlert('Something went wrong')
+if (paramsRes === "true") {
+  showAlert("Todo was updated successfully");
+} else if (paramsRes === "false") {
+  showAlert("Something went wrong");
 }
 
 // form submit
@@ -25,45 +24,6 @@ formEl.addEventListener("submit", (e) => {
     title: form.get("title"),
     text: form.get("text"),
   }).then((res) => {
-    appendTodo(res);
+    appendTodo(todosListEl, res);
   });
 });
-
-function appendTodo({ id, title, text }) {
-  const li = document.createElement("li");
-  const div = document.createElement("div");
-  const span = document.createElement("span");
-  const btn = document.createElement("button");
-  const btnEdit = document.createElement("button");
-  const p = document.createElement('p');
-
-  p.textContent = text;
-
-  div.className = "content";
-  div.innerHTML = `<h4>${title}</h4>`;
-
-  btnEdit.innerHTML = "&#x270E";
-  btn.innerHTML = "&times;";
-
-  btn.setAttribute("data-id", id);
-  btnEdit.setAttribute("data-id", id);
-  btn.addEventListener("click", (e) => {
-    const todoId = e.target.getAttribute("data-id");
-    deleteTodo(todoId).then((res) => {
-      btn.removeEventListener("click", () => {});
-      btn.closest('li').remove();
-    });
-  });
-
-  span.append(btnEdit);
-  span.append(btn);
-  div.append(span);
-  li.append(div);
-  li.append(p);
-
-  btnEdit.addEventListener("click", (e) => {
-    window.location.href = 'edit.html?id=' + e.target.getAttribute('data-id')
-  });
-
-  todosListEl.append(li);
-}
